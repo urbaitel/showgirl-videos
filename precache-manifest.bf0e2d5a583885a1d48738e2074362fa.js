25e9fc24e5f1e0483a47d69608841582a6ccd34a
Marc Urbaitel <marc@urbaitel.com>
2020-05-02 10:18:11 -0700

    Updates

---
+++ precache-manifest.bf0e2d5a583885a1d48738e2074362fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20e269a138bb6007fd07367ca57587ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a63bdf1f9a4c70bc812",
+    "url": "/static/css/main.3421dd0e.chunk.css"
+  },
+  {
+    "revision": "d01cf4ad91376a77998b",
+    "url": "/static/js/2.fe733c3f.chunk.js"
+  },
+  {
+    "revision": "89e6779be8c35e87894daedfd6ed62b2",
+    "url": "/static/js/2.fe733c3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a63bdf1f9a4c70bc812",
+    "url": "/static/js/main.6f00e7eb.chunk.js"
+  },
+  {
+    "revision": "1e183e214749f02271d2",
+    "url": "/static/js/runtime-main.50e8d5ae.js"
+  },
+  {
+    "revision": "2c915f618313a25e2eac47df70872652",
+    "url": "/static/media/logo.2c915f61.jpg"
+  }
+]);
